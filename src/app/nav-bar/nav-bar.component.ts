f8a6c36b8dfb958483841ce5ee293f1e87c82872
lakshmipavani <pavani.kota@quantela.com>
2017-11-03 04:45:17 +0530

    nav bar css changes.

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -11,5 +11,9 @@ export class NavBarComponent implements OnInit {
 
   ngOnInit() {
   }
+  $('.nav').on('click', 'li', function(){
+    $('.nav li').removeClass('active');
+    $(this).addClass('active');
+});
 
 }
