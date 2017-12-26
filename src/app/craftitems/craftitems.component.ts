90caa51a3b46f1b2b6dffa9da879834b9c92acd9
lakshmipavani <pavani.kota@quantela.com>
2017-12-26 18:30:00 +0530

    added few assets and shopmenu hover events.

--- src/app/craftitems/craftitems.component.ts
+++ src/app/craftitems/craftitems.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild,ViewContainerRef,ComponentFactoryResolver } from '@angular/core';
 
 @Component({
   selector: 'app-craftitems',
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./craftitems.component.css']
 })
 export class CraftitemsComponent implements OnInit {
-
-  constructor() { }
-
+   _itemHovered;
+  constructor(public viewContainerRef: ViewContainerRef) {
+  }
   ngOnInit() {
   }
-
+  hoverView(event){
+     console.log(event+"hover event.");
+     //this.shopContainer.createComponent(craftitemsComponent);
+     // event.preventDefault();
+     if(event.type == "mouseover"){
+         let target = event.target;
+         this._itemHovered = event.target.id;
+     }else{
+         let target = event.target;
+         this._itemHovered = false;
+         //target.style.background = 'none';
+     }
+  }
+  showDialog(){
+    console.log(event+"editted/deleted event.");
+  }
 }
