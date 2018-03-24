a1e1da2d3d646c3eb2c7e8c09092233a9e866fb9
lakshmipavani <pavani.kota@quantela.com>
2018-03-24 20:17:00 +0530

    Integrated leaflet maps.

--- src/app/main-view/main-view.component.ts
+++ src/app/main-view/main-view.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var L: any;
 @Component({
   selector: 'app-main-view',
   templateUrl: './main-view.component.html',
@@ -10,6 +10,13 @@ export class MainViewComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+    L.tileLayer('https://api.mapbox.com/styles/v1/timtuity/citdzqqoz009d2jo6gfxz29nf/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
+	    attribution: 'Map data &copy;',
+	    maxZoom: 18,
+	    id: 'mapbox.satellite',
+	    accessToken: 'pk.eyJ1IjoidGltdHVpdHkiLCJhIjoicm5rOHVFSSJ9.9Zl65ucEq3ClmtRo1hHGRw'
+   }).addTo(mymap);
   }
 
 }
